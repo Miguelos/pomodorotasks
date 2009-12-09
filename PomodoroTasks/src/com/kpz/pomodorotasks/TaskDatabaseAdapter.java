a6524b3e67526390ad43b3ea89a15b2bb89b68ba
kavanpdx <kavanpdx@5e04498e-d236-11de-97bf-6350044336de>
2009-12-09 06:17:35 +0000

    Added option to change task duration

--- PomodoroTasks/src/com/kpz/pomodorotasks/TaskDatabaseAdapter.java
+++ PomodoroTasks/src/com/kpz/pomodorotasks/TaskDatabaseAdapter.java
@@ -26,225 +26,261 @@ import android.util.Log;
 
 public class TaskDatabaseAdapter {
 
+	private DatabaseHelper mDbHelper;
+	private SQLiteDatabase mDb;
+
 	private static final String TAG = "PomodoroTasks";
+
+	private static final String DATABASE_NAME = "pomodorotasks";
 	
-    public static final String KEY_DESCRIPTION = "description";
-    public static final String KEY_SEQUENCE = "sequence";
-    public static final String KEY_ROWID = "_id";
-    public static final String KEY_STATUS = "status";
-    public static final String[] SELECTION_KEYS = {KEY_ROWID, KEY_DESCRIPTION, KEY_SEQUENCE, KEY_STATUS};
-    
-    enum StatusType {
-    	
-    	OPEN("Open"),
-    	COMPLETED("Completed");
-    	
-    	private String description;
+	private static final String TASKS_TABLE = "task";
+	public static final String KEY_DESCRIPTION = "description";
+	public static final String KEY_SEQUENCE = "sequence";
+	public static final String KEY_ROWID = "_id";
+	public static final String KEY_STATUS = "status";
+	public static final String[] SELECTION_KEYS = { KEY_ROWID, KEY_DESCRIPTION, KEY_SEQUENCE, KEY_STATUS };
+	
+	private static final String CONFIG_TABLE = "config";
+	public static final String KEY_CONFIG_NAME = "name";	
+	public static final String KEY_CONFIG_VALUE = "value";	
+
+	enum StatusType {
+
+		OPEN("Open"), COMPLETED("Completed");
+
+		private String description;
 
 		private StatusType(String desc) {
-    		this.setDescription(desc);
-		}
-
-		public void setDescription(String description) {
-			this.description = description;
+			this.description = desc;
 		}
 
 		public String getDescription() {
 			return description;
 		}
-		
-		public static boolean isOpen(String statusDesc){
+
+		public static boolean isOpen(String statusDesc) {
 			return OPEN.description.equals(statusDesc);
 		}
-		
-		public static boolean isCompleted(String statusDesc){
+
+		public static boolean isCompleted(String statusDesc) {
 			return COMPLETED.description.equals(statusDesc);
 		}
-    };
-    
-    
-    
-    private DatabaseHelper mDbHelper;
-    private SQLiteDatabase mDb;
-    
-    private static final String DATABASE_TABLE = "tasks";
-    private static final String DATABASE_NAME = "pomodorotasks";
-    
-    private static final String DATABASE_CREATE =
-            "create table " + DATABASE_TABLE + " (_id integer primary key autoincrement"
-    				+ ", sequence integer"
-    				+ ", status text not null default Open"
-    				+ ", description text not null);";
+	};
+	
+	enum ConfigType {
 
-    private static final int DATABASE_VERSION = 3;
+		TIME_DURATION("25");
 
-    private final Context mCtx;
+		private String defaultValue;
 
-    private static class DatabaseHelper extends SQLiteOpenHelper {
+		private ConfigType(String defaultVal) {
+			this.defaultValue = defaultVal;
+		}
 
-        DatabaseHelper(Context context) {
-            super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        }
+		public String getDefaultValue() {
+			return defaultValue;
+		}
+	};
 
-        @Override
-        public void onCreate(SQLiteDatabase db) {
+	private static final String [] DATABASE_CREATE_LIST = 
+					{"create table " + TASKS_TABLE 
+									+ " (_id integer primary key autoincrement" + ", sequence integer" + ", status text not null default Open" + ", description text not null);" 
+				     , "create table " + CONFIG_TABLE 
+				  					+ " (_id integer primary key autoincrement" + ", name text" + ", value text not null);"
+				     , "insert into " + CONFIG_TABLE 
+				  					+ " (name, value) VALUES ('" + ConfigType.TIME_DURATION + "', '" + ConfigType.TIME_DURATION.defaultValue + "');"}; 					
+				  					;
 
-            db.execSQL(DATABASE_CREATE);
-        }
+	private static final int DATABASE_VERSION = 3;
 
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
-                    + newVersion + ", which will destroy all old data");
-            db.execSQL("DROP TABLE IF EXISTS tasks");
-            onCreate(db);
-        }
-    }
+	private final Context mCtx;
 
-    /**
-     * Constructor - takes the context to allow the database to be
-     * opened/created
-     * 
-     * @param ctx the Context within which to work
-     */
-    public TaskDatabaseAdapter(Context ctx) {
-        this.mCtx = ctx;
-    }
+	private static class DatabaseHelper extends SQLiteOpenHelper {
 
-    /**
-     * Open the tasks database. If it cannot be opened, try to create a new
-     * instance of the database. If it cannot be created, throw an exception to
-     * signal the failure
-     * 
-     * @return this (self reference, allowing this to be chained in an
-     *         initialization call)
-     * @throws SQLException if the database could be neither opened or created
-     */
-    public TaskDatabaseAdapter open() throws SQLException {
-        mDbHelper = new DatabaseHelper(mCtx);
-        mDb = mDbHelper.getWritableDatabase();
-        return this;
-    }
-    
-    public void close() {
-        mDbHelper.close();
-    }
+		DatabaseHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
 
+		@Override
+		public void onCreate(SQLiteDatabase db) {
 
-    public long createTask(String description) {
-        ContentValues initialValues = new ContentValues();
-        initialValues.put(KEY_DESCRIPTION, description);
+			Log.d(TAG, "create stat: " + DATABASE_CREATE_LIST);
+			for (String sql : DATABASE_CREATE_LIST) {
+				db.execSQL(sql);
+			}
+		}
 
-        long taskId = mDb.insert(DATABASE_TABLE, null, initialValues);
-        ContentValues args = new ContentValues();
-        args.put(KEY_SEQUENCE, taskId);
-        mDb.update(DATABASE_TABLE, args, KEY_ROWID + "=" + taskId, null);
-        
-		return taskId;
-    }
-
-    public boolean delete(long rowId) {
-
-        return mDb.delete(DATABASE_TABLE, KEY_ROWID + "=" + rowId, null) > 0;
-    }
-    
-	public boolean deleteAll() {
-        return mDb.delete(DATABASE_TABLE, null, null) > 0;
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			Log.w(TAG, "Upgrading database from version " + oldVersion + " to " + newVersion + ", which will destroy all old data");
+			db.execSQL("DROP TABLE IF EXISTS " + TASKS_TABLE);
+			db.execSQL("DROP TABLE IF EXISTS " + CONFIG_TABLE);
+			onCreate(db);
+		}
 	}
 
-    /**
-     * Return a Cursor over the list of all tasks in the database
-     * 
-     * @return Cursor over all tasks
-     */
-    public Cursor fetchAll() {
+	/**
+	 * Constructor - takes the context to allow the database to be
+	 * opened/created
+	 * 
+	 * @param ctx
+	 *            the Context within which to work
+	 */
+	public TaskDatabaseAdapter(Context ctx) {
+		this.mCtx = ctx;
+	}
 
-        return mDb.query(DATABASE_TABLE, SELECTION_KEYS, null, null, null, null, "sequence");
-    }
+	/**
+	 * Open the tasks database. If it cannot be opened, try to create a new
+	 * instance of the database. If it cannot be created, throw an exception to
+	 * signal the failure
+	 * 
+	 * @return this (self reference, allowing this to be chained in an
+	 *         initialization call)
+	 * @throws SQLException
+	 *             if the database could be neither opened or created
+	 */
+	public TaskDatabaseAdapter open() throws SQLException {
+		mDbHelper = new DatabaseHelper(mCtx);
+		mDb = mDbHelper.getWritableDatabase();
+		return this;
+	}
 
-    /**
-     * Return a Cursor positioned at the task that matches the given rowId
-     * 
-     * @param rowId id of task to retrieve
-     * @return Cursor positioned to matching task, if found
-     * @throws SQLException if task could not be found/retrieved
-     */
-    public Cursor fetch(long rowId) throws SQLException {
+	public void close() {
+		mDbHelper.close();
+	}
 
-        Cursor mCursor =
+	public long createTask(String description) {
+		ContentValues initialValues = new ContentValues();
+		initialValues.put(KEY_DESCRIPTION, description);
 
-                mDb.query(true, DATABASE_TABLE, SELECTION_KEYS, KEY_ROWID + "=" + rowId, null,
-                        null, null, null, null);
-        if (mCursor != null) {
-            mCursor.moveToFirst();
-        }
-        return mCursor;
+		long taskId = mDb.insert(TASKS_TABLE, null, initialValues);
+		ContentValues args = new ContentValues();
+		args.put(KEY_SEQUENCE, taskId);
+		mDb.update(TASKS_TABLE, args, KEY_ROWID + "=" + taskId, null);
 
-    }
+		return taskId;
+	}
 
-    public boolean update(long rowId, String description) {
-        ContentValues args = new ContentValues();
-        args.put(KEY_DESCRIPTION, description);
+	public boolean delete(long rowId) {
 
-        return mDb.update(DATABASE_TABLE, args, KEY_ROWID + "=" + rowId, null) > 0;
-    }
+		return mDb.delete(TASKS_TABLE, KEY_ROWID + "=" + rowId, null) > 0;
+	}
+
+	public boolean deleteAll() {
+		return mDb.delete(TASKS_TABLE, null, null) > 0;
+	}
+
+	/**
+	 * Return a Cursor over the list of all tasks in the database
+	 * 
+	 * @return Cursor over all tasks
+	 */
+	public Cursor fetchAll() {
+
+		return mDb.query(TASKS_TABLE, SELECTION_KEYS, null, null, null, null, "sequence");
+	}
+
+	/**
+	 * Return a Cursor positioned at the task that matches the given rowId
+	 * 
+	 * @param rowId
+	 *            id of task to retrieve
+	 * @return Cursor positioned to matching task, if found
+	 * @throws SQLException
+	 *             if task could not be found/retrieved
+	 */
+	public Cursor fetch(long rowId) throws SQLException {
+
+		Cursor mCursor =
+
+		mDb.query(true, TASKS_TABLE, SELECTION_KEYS, KEY_ROWID + "=" + rowId, null, null, null, null, null);
+		if (mCursor != null) {
+			mCursor.moveToFirst();
+		}
+		return mCursor;
+
+	}
+
+	public boolean update(long rowId, String description) {
+		ContentValues args = new ContentValues();
+		args.put(KEY_DESCRIPTION, description);
+
+		return mDb.update(TASKS_TABLE, args, KEY_ROWID + "=" + rowId, null) > 0;
+	}
 
 	public boolean updateStatus(int rowId, boolean completed) {
 
-        ContentValues args = new ContentValues();
-        args.put(KEY_STATUS, completed ? StatusType.COMPLETED.getDescription() : StatusType.OPEN.getDescription());
+		ContentValues args = new ContentValues();
+		args.put(KEY_STATUS, completed ? StatusType.COMPLETED.getDescription() : StatusType.OPEN.getDescription());
 
-        return mDb.update(DATABASE_TABLE, args, KEY_ROWID + "=" + rowId, null) > 0;
+		return mDb.update(TASKS_TABLE, args, KEY_ROWID + "=" + rowId, null) > 0;
 	}
-	
-	public void move(int fromSeq, int toRowId,
-			int toSeq) {
 
-		
-		if (fromSeq == toSeq){
+	public void move(int fromSeq, int toRowId, int toSeq) {
+
+		if (fromSeq == toSeq) {
 			return;
 		}
-		
-		if(fromSeq < toSeq){
-			
+
+		if (fromSeq < toSeq) {
+
 			ContentValues args = new ContentValues();
-	        args.put(KEY_SEQUENCE, toSeq);
-	        mDb.update(DATABASE_TABLE, args, KEY_SEQUENCE + "=" + fromSeq, null);
-	        
-	        
-	        for (int i = 0; i <  toSeq - fromSeq - 1; i++) {
-	        	
-	            int fromSeq1 = fromSeq + i + 1;
-	            int toSeq1 = fromSeq1 - 1;
-	        	args = new ContentValues();
-	        	args.put(KEY_SEQUENCE, toSeq1);
-				mDb.update(DATABASE_TABLE, args, KEY_SEQUENCE + "=" + fromSeq1, null);	
+			args.put(KEY_SEQUENCE, toSeq);
+			mDb.update(TASKS_TABLE, args, KEY_SEQUENCE + "=" + fromSeq, null);
+
+			for (int i = 0; i < toSeq - fromSeq - 1; i++) {
+
+				int fromSeq1 = fromSeq + i + 1;
+				int toSeq1 = fromSeq1 - 1;
+				args = new ContentValues();
+				args.put(KEY_SEQUENCE, toSeq1);
+				mDb.update(TASKS_TABLE, args, KEY_SEQUENCE + "=" + fromSeq1, null);
 			}
-	        
-	        args = new ContentValues();
-	    	args.put(KEY_SEQUENCE, toSeq - 1);
-			mDb.update(DATABASE_TABLE, args, KEY_ROWID + "=" + toRowId, null);
-			
+
+			args = new ContentValues();
+			args.put(KEY_SEQUENCE, toSeq - 1);
+			mDb.update(TASKS_TABLE, args, KEY_ROWID + "=" + toRowId, null);
+
 		} else {
-		
-	        ContentValues args = new ContentValues();
-	        args.put(KEY_SEQUENCE, toSeq);
-	        mDb.update(DATABASE_TABLE, args, KEY_SEQUENCE + "=" + fromSeq, null);
-	        
-	        
-	        for (int i = 0; i < fromSeq - toSeq - 1; i++) {
-	        	
-	            int fromSeq1 = fromSeq - i - 1;
-	            int toSeq1 = fromSeq - i;
-	        	args = new ContentValues();
-	        	args.put(KEY_SEQUENCE, toSeq1);
-				mDb.update(DATABASE_TABLE, args, KEY_SEQUENCE + "=" + fromSeq1, null);	
+
+			ContentValues args = new ContentValues();
+			args.put(KEY_SEQUENCE, toSeq);
+			mDb.update(TASKS_TABLE, args, KEY_SEQUENCE + "=" + fromSeq, null);
+
+			for (int i = 0; i < fromSeq - toSeq - 1; i++) {
+
+				int fromSeq1 = fromSeq - i - 1;
+				int toSeq1 = fromSeq - i;
+				args = new ContentValues();
+				args.put(KEY_SEQUENCE, toSeq1);
+				mDb.update(TASKS_TABLE, args, KEY_SEQUENCE + "=" + fromSeq1, null);
 			}
-	        
-	        args = new ContentValues();
-	    	args.put(KEY_SEQUENCE, toSeq + 1);
-			mDb.update(DATABASE_TABLE, args, KEY_ROWID + "=" + toRowId, null);
-		
+
+			args = new ContentValues();
+			args.put(KEY_SEQUENCE, toSeq + 1);
+			mDb.update(TASKS_TABLE, args, KEY_ROWID + "=" + toRowId, null);
+
 		}
+	}
+
+	public int fetchTaskDurationSetting() {
+		
+		Cursor cursor = mDb.query(true, CONFIG_TABLE, new String[] {KEY_CONFIG_VALUE}, KEY_CONFIG_NAME + "= '" +  ConfigType.TIME_DURATION.name() + "'", null, null, null, null, null);
+
+		if (cursor != null) {
+			cursor.moveToFirst();
+		}
+
+		return Integer.parseInt(cursor.getString(cursor.getColumnIndex(KEY_CONFIG_VALUE)));
+	}
+
+	public boolean updateTaskDurationSetting(int progress) {
+
+		ContentValues args = new ContentValues();
+		args.put(KEY_CONFIG_VALUE, Integer.toString(progress));
+
+		return mDb.update(CONFIG_TABLE, args, KEY_CONFIG_NAME + "= '" + ConfigType.TIME_DURATION.name() + "'", null) > 0;
+
 	}
 }
